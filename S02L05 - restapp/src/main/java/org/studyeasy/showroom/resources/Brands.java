db44e6b378905c50008990badf09e9a7eb481334
studyeasy <45162959+studyeasy@users.noreply.github.com>
2018-11-19 15:08:05 +0530

    Add files via upload

---
+++ S02L05 - restapp/src/main/java/org/studyeasy/showroom/resources/Brands.java
@@ -0,0 +1,16 @@
+package org.studyeasy.showroom.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/showroom")
+public class Brands {
+    @GET
+	@Path("/getBrands")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getBrands() {
+		return "list of brands";
+	}
+}
