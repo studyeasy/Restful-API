6e7bedac2c0307433819af1972e2bbac1e7386bb
studyeasy <45162959+studyeasy@users.noreply.github.com>
2019-04-06 13:31:48 +0530

    Add files via upload

---
+++ S06L02 - Client building steps/src/main/java/org/studyeasy/App.java
@@ -0,0 +1,27 @@
+package org.studyeasy;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("app")
+public class App {
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String demo() {    
+
+         Client client = ClientBuilder.newClient();
+         String response =  client.target("http://localhost:8080/restapp/showroom/brands")
+        		 .request(MediaType.APPLICATION_JSON)
+        		 .get(String.class); 
+         return response;
+	}
+
+}
